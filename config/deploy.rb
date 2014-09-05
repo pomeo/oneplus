aae6d7228efa139c534fc7622afbbac5ce6f8b57
Sergey Ovechkin <me@sovechkin.com>
2014-09-05 13:25:21 +0400

    FIX ссылка на репозиторий

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application, "oneplus"
 #========================
 require           "capistrano-offroad"
 offroad_modules   "defaults", "supervisord"
-set :repository,  "git@github.com:user/#{application}.git"
+set :repository,  "git@github.com:pomeo/#{application}.git"
 set :supervisord_start_group, "pomeo"
 set :supervisord_stop_group, "pomeo"
 set :deploy_to, "/home/pomeo/www"
