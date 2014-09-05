04bea6e2db3aeee325e5251696ec0bd487bc3146
Sergey Ovechkin <me@sovechkin.com>
2014-09-05 13:22:16 +0400

    добавляем компиляцию go

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,4 +18,11 @@ set :deploy_group,"pomeo"
 #========================
 role :app,        "oneplus.sovechkin.com"
 
-after "deploy:create_symlink", "deploy:npm_install", "deploy:restart"
+after "deploy:create_symlink", "deploy:go_compile", "deploy:restart"
+
+namespace :deploy do
+    desc "Compile"
+    task :go_compile do
+      run "cd #{current_path} && go build src/oneplus.go"
+    end
+  end
